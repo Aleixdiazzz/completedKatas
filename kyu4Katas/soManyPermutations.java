d6d2837b4e5c24a9156cd60708027246f7089692
Aleix Díaz <107421937+Aleixdiazzz@users.noreply.github.com>
2024-02-08 19:10:16 +0100

    Create soManyPermutations.java

---
+++ kyu4Katas/soManyPermutations.java
@@ -0,0 +1,91 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+class Permutations 
+{   
+  static ArrayList<String> result = new ArrayList();
+  
+  public static List<String> singlePermutations(String s) 
+  {
+    result.clear();
+    
+    permutation(s, "");
+    
+    result.removeIf(n -> (n.length() != s.length()));
+    
+    Set<String> no_duplicates = new HashSet<String>(result);
+    result.clear();
+    result.addAll(no_duplicates);
+    return result;
+  }
+  
+  public static void permutation(String str, String ans) 
+   {
+ 
+        if (str.length() == 0) {
+            result.add(ans);
+            return;
+        }
+ 
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) +
+                        str.substring(i + 1);
+ 
+            permutation(ros, ans + ch);
+        }
+    }
+}
+yesterdayRefactorDiscuss
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+class Permutations 
+{   
+  static ArrayList<String> result = new ArrayList();
+  
+  public static List<String> singlePermutations(String s) 
+  {
+    //To clear last results
+    result.clear();
+    
+    permutation(s, "");
+    
+    //Removing strings with a minor length than 's'
+    result.removeIf(n -> (n.length() != s.length()));
+    
+    //Removing duplicates
+    Set<String> no_duplicates = new HashSet<String>(result);
+    result.clear();
+    result.addAll(no_duplicates);
+    return result;
+  }
+  
+  public static void permutation(String str, String ans) 
+   {
+ 
+        // If string is empty
+        if (str.length() == 0) {
+            result.add(ans);
+            return;
+        }
+ 
+        for (int i = 0; i < str.length(); i++) {
+ 
+            // ith character of str
+            char ch = str.charAt(i);
+ 
+            // Rest of the string after excluding
+            // the ith character
+            String ros = str.substring(0, i) +
+                        str.substring(i + 1);
+ 
+            // Recursive call
+            permutation(ros, ans + ch);
+        }
+    }
+}
