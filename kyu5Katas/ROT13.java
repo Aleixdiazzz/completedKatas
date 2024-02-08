d69afb80bfc891488f91cd67c946c85f408b51c7
Aleix Díaz <107421937+Aleixdiazzz@users.noreply.github.com>
2024-02-08 19:19:38 +0100

    Create ROT13.java

---
+++ kyu5Katas/ROT13.java
@@ -0,0 +1,18 @@
+class Solution {
+  
+   public static String rot13(String message) {
+     return (MYrot13(message));
+   }
+   public static String MYrot13(String input) {
+   StringBuilder sb = new StringBuilder();
+   for (int i = 0; i < input.length(); i++) {
+       char c = input.charAt(i);
+       if       (c >= 'a' && c <= 'm') c += 13;
+       else if  (c >= 'A' && c <= 'M') c += 13;
+       else if  (c >= 'n' && c <= 'z') c -= 13;
+       else if  (c >= 'N' && c <= 'Z') c -= 13;
+       sb.append(c);
+   }
+   return sb.toString();
+  }
+}
