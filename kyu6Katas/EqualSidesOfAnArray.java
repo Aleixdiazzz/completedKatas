281a477ac60cd8a1fea38fb9ba4333c8f6d9ab7f
Aleix Díaz <107421937+Aleixdiazzz@users.noreply.github.com>
2024-02-09 16:22:30 +0100

    Update EqualSidesOfAnArray.java

--- kyu6Katas/EqualSidesOfAnArray.java
+++ kyu6Katas/EqualSidesOfAnArray.java
@@ -1,1 +1,26 @@
+public class Kata {
+  public static int findEvenIndex(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+            if (sumOfLeft(arr, i) == sumOfRight(arr, i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    private static int sumOfLeft(int[] arr, int index) {
+        int sum = 0;
+        for (int i = 0; i < index; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    private static int sumOfRight(int[] arr, int index) {
+        int sum = 0;
+        for (int i = index + 1; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
